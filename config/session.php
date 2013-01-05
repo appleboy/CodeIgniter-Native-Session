938ccbb8235113b132e34089be28db75e1034205
Marko Martinović <marko@techytalk.info>
2013-01-05 12:42:19 +0100

    Default namespace to blank

--- config/session.php
+++ config/session.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['sess_namespace'] = 'thumb_machine';
+$config['sess_namespace'] = '';
 
 /* End of file session.php */
 /* Location: ./application/config/session.php */
